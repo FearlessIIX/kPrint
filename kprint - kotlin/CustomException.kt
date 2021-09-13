8fa77c99eb97ccd28dcc44ca8cfd3ac1cdc418e9
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2021-09-13 13:03:00 -0400

    Finished CustomException.kt

--- kprint - kotlin/CustomException.kt
+++ kprint - kotlin/CustomException.kt
@@ -2,4 +2,4 @@ package kprint
 
 class InvalidCoordinateException(message : String) : Exception(message) {  }
 class InvalidSizeException(message : String) : Exception(message) {  }
-class InvalidStringSizeExeption(message : String) : Exception(message) {  }
+class InvalidStringSizeException(message : String) : Exception(message) {  }
